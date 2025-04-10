f6a88a49f5cfe2fee093809b0b7ff4232b3a54ab
Ktoto198 <frybenderer43@gmail.com>
2025-04-10 08:59:02 +0200

    start of repository

---
+++ ResSim/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace ResSim.ViewModels;
+
+public class ViewModelBase : ObservableObject
+{
+}
---
+++ ResSim/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace ResSim.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
